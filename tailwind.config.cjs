4bfa7c05232ad6d5cfebc1b70236ff4aed827be2
Lukas Trombach <TrombachL@gmail.com>
2023-10-14 10:33:45 +1300

    use prettier for formatting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '976px',
-			xl: '1440px',
-		},
-		colors: {
-			'transparent': 'transparent',
-			'black': '#13151a',
-			'white': 'whitesmoke',
-			'blue': '#007ce0',
-		},
-		extend: {
-			borderRadius: {
-				'4xl': '2rem',
-			}
-		},
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      transparent: "transparent",
+      black: "#13151a",
+      white: "whitesmoke",
+      blue: "#007ce0",
+    },
+    extend: {
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
