e18b2bb0eaa90366db6348c0c9da39626f985673
Lukas Trombach <TrombachL@gmail.com>
2023-10-24 21:37:15 +1300

    update nav links, TODO: fix mobile view

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,13 +8,6 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
-    colors: {
-      transparent: "transparent",
-      black: "#13151a",
-      white: "whitesmoke",
-      blue: "#007ce0",
-      gray: "rgba(205,217,229,0.25)",
-    },
     extend: {
       borderRadius: {
         "4xl": "2rem",
