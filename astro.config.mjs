8260ab016f313474d1c521c5fc08c0c1b48ed0ad
Lukas Trombach <TrombachL@gmail.com>
2024-01-07 17:20:33 +0100

    add astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), icon()],
   image: {
-    domains: ["placehold.co"],
+    domains: ["placehold.co"]
   },
   prefetch: {
-    prefetchAll: true,
-  },
+    prefetchAll: true
+  }
 });
