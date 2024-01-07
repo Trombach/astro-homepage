cd2ac7fd479cff79b76b21664f3ce2a6eba69f48
Lukas Trombach <TrombachL@gmail.com>
2024-01-07 23:34:03 +0100

    first version of tech grid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,39 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), icon()],
+  integrations: [
+    tailwind(),
+    svelte(),
+    icon({
+      include: {
+        logos: [
+          "typescript-icon",
+          "angular-icon",
+          "javascript",
+          "html-5",
+          "css-3",
+          "tailwindcss-icon",
+          "nextjs-icon",
+          "git-icon",
+          "github-icon",
+          "go",
+          "bash-icon",
+          "rust",
+          "astro-icon",
+          "nodejs-icon",
+          "svelte-icon",
+        ],
+      },
+    }),
+  ],
   image: {
-    domains: ["placehold.co"]
+    domains: ["placehold.co"],
   },
   prefetch: {
-    prefetchAll: true
-  }
+    prefetchAll: true,
+  },
 });
