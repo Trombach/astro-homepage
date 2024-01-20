34da50e7493d67049c681bac7182169e131a5443
Lukas Trombach <TrombachL@gmail.com>
2024-01-20 14:09:55 +1300

    migrate to unplugin-icons
    - remove astro-icon dependencies and code
    - update all icons to unplugin version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,23 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import icon from "astro-icon";
-
+import Icons from "unplugin-icons/vite";
 import lighthouse from "astro-lighthouse";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), icon({
-    include: {
-      logos: ["typescript-icon", "angular-icon", "javascript", "html-5", "css-3", "tailwindcss-icon", "nextjs-icon", "git-icon", "github-icon", "go", "bash-icon", "rust", "astro-icon", "nodejs-icon", "svelte-icon"]
-    }
-  }), lighthouse()],
+  integrations: [tailwind(), svelte(), lighthouse()],
   image: {
     domains: ["placehold.co"]
   },
   prefetch: {
     prefetchAll: true
+  },
+  vite: {
+    plugins: [
+      Icons({
+        compiler: "astro"
+      })
+    ]
   }
 });
