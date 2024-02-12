dbcf8665f4bd0467943fc718d0bb8aef899a35c6
Lukas <TrombachL@gmail.com>
2024-02-12 22:55:23 +1300

    remove astro-lighthouse and astro-toc (#22)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import Icons from "unplugin-icons/vite";
-import lighthouse from "astro-lighthouse";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), lighthouse(), mdx()],
+  integrations: [tailwind(), svelte(), mdx()],
   image: {
     domains: ["placehold.co"]
   },
