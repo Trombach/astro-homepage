212edfad7f37c2baa704aef536694deaa38b0952
Lukas Trombach <TrombachL@gmail.com>
2024-02-12 22:19:14 +1300

    improve hero grid
    - prettify animation by using svelte fly transition
    - remove unnecessary "transitioning" state

--- src/components/Hero/Panel.svelte
+++ src/components/Hero/Panel.svelte
@@ -1,59 +1,55 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
-  import { fade } from "svelte/transition";
+  import { fly } from "svelte/transition";
   import PlusIcon from "icons:svelte/heroicons/plus";
+  import { type PanelNumber } from "./HeroGrid.svelte";
 
   export let expanded: boolean;
-  export let number: number;
-  export let transitioning: boolean;
+  export let number: PanelNumber;
   export let title: string;
-  export let href: string;
+  export let transitionDirection: "left" | "right";
 
-  const dispatch = createEventDispatcher<{ hasExpanded: number }>();
+  let direction = 1;
+
+  const dispatch = createEventDispatcher<{ hasExpanded: PanelNumber }>();
 
   const toggleExpanded = () => {
     expanded = true;
-    transitioning = true;
     dispatch("hasExpanded", number);
   };
+
+  $: transitionDirection === "left" ? (direction = 1) : (direction = -1);
 </script>
 
 <div
-  class="flex items-center justify-end rounded-[50cqh] border border-border bg-card text-card-foreground backdrop-blur supports-[backdrop-filter]:bg-background/60 lg:h-2/3 lg:flex-col lg:rounded-[50cqw]"
+  class="relative rounded-[50cqh] border border-border bg-card text-card-foreground backdrop-blur supports-[backdrop-filter]:bg-background/60 lg:h-2/3 lg:rounded-[50cqw] overflow-clip"
   data-panel
   data-expanded={expanded || null}
   class:flex-col={expanded}
 >
-  {#if expanded && !transitioning}
-    <h1 class="mb-auto">
-      {title}
-    </h1>
-    <div in:fade>
-      <slot name="panel" />
-    </div>
-  {:else if !transitioning}
-    <div in:fade class="lg:[writing-mode:_vertical-lr]">
-      {title}
-    </div>
-  {/if}
+  {#key expanded}
+    <div
+      in:fly={{ delay: 500, x: 300 * direction }}
+      out:fly={{ x: -300 * direction }}
+      class="absolute inset-0"
+      class:small-panel={!expanded}
+    >
+      {#if expanded}
+        <slot name="panel" />
+      {:else}
+        <div class="lg:[writing-mode:_vertical-lr]">
+          {title}
+        </div>
 
-  {#if !expanded && !transitioning}
-    <button
-      in:fade
-      class="m-3 rounded-full border border-border bg-background p-3"
-      on:click={toggleExpanded}
-    >
-      <PlusIcon />
-    </button>
-  {:else if !transitioning}
-    <a
-      in:fade
-      {href}
-      class="m-3 rounded-full border border-border bg-background p-3"
-    >
-      {title}
-    </a>
-  {/if}
+        <button
+          class="m-3 rounded-full border border-border bg-background p-3"
+          on:click={toggleExpanded}
+        >
+          <PlusIcon />
+        </button>
+      {/if}
+    </div>
+  {/key}
 </div>
 
 <style lang="postcss">
@@ -63,6 +59,10 @@
 
   [data-panel][data-expanded="true"] {
     @apply rounded-4xl;
-    transition: border-radius 250ms -250ms;
+    transition: border-radius 250ms ease-in-out -250ms;
+  }
+
+  .small-panel {
+    @apply flex items-center justify-end lg:flex-col;
   }
 </style>
