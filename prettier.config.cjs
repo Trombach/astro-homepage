232bf46073c4fc1f24af8a9ca940cce764d73bdc
Lukas Trombach <TrombachL@gmail.com>
2024-02-17 14:28:52 +1300

    fix formatting

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,5 +2,9 @@
 
 /** @type import('prettier').Config */
 module.exports = {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss", "prettier-plugin-astro-organize-imports"]
+  plugins: [
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-astro-organize-imports",
+  ],
 };
