4a37ee6b12f322316470e4aca6e20ea25eacd8dc
Lukas Trombach <TrombachL@gmail.com>
2024-02-19 00:02:29 +1300

    add server function for gh contributions

---
+++ src/utils/fetchHelper.ts
@@ -0,0 +1,35 @@
+/* global URL RequestInit fetch URLSearchParams*/
+
+import type { z } from "astro/zod";
+
+export default async function fetchWithSchema<T>(
+  schema: z.ZodType<T>,
+  input: URL | string,
+  init?: RequestInit,
+) {
+  if (typeof input === "string") {
+    input = getURL(input);
+  }
+
+  const data = await fetch(input, init);
+  return schema.parse(await data.json());
+}
+
+function getURL(url: URL | string, searchParams?: { [key: string]: string }) {
+  if (typeof url === "string" && url.startsWith("http")) {
+    url = new URL(url);
+  } else {
+    const base = import.meta.env.VERCEL_URL
+      ? `https://${import.meta.env.VERCEL_URL}`
+      : "http://localhost:4321";
+    url = new URL(url, base);
+  }
+
+  if (searchParams) {
+    url = new URL(
+      `${url.origin}${url.pathname}${new URLSearchParams({ ...url.searchParams, ...searchParams })}`,
+    );
+  }
+
+  return url;
+}
