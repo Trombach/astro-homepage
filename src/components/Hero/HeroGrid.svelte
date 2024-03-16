879bd1d421ce764790f253435bb1f75312b70a92
Lukas Trombach <me@lukastrombach.dev>
2024-03-16 22:53:17 +1300

    refactor hero grid to use shared store for state

--- src/components/Hero/HeroGrid.svelte
+++ src/components/Hero/HeroGrid.svelte
@@ -1,70 +1,59 @@
 <script context="module" lang="ts">
+  import { derived, writable } from "svelte/store";
+
   const panels = [1, 2, 3, 4] as const satisfies number[];
   export type PanelState = "expanded" | "start" | "end";
   export type PanelNumber = (typeof panels)[number];
-</script>
+  export type PanelStates = { [key in PanelNumber]: PanelState };
 
-<script lang="ts">
-  /* global CustomEvent */
-
-  import Panel from "./Panel.svelte";
-
-  const panelStates: { [key in PanelNumber]: PanelState } = {
+  const defaultPanelStates = {
     1: "expanded",
     2: "end",
     3: "end",
     4: "end",
-  };
+  } as const satisfies PanelStates;
 
-  const toggleOthers = ({
-    detail: currentExpanded,
-  }: CustomEvent<PanelNumber>) => {
-    panels
-      .filter((panel) => panel !== currentExpanded)
-      .forEach(
-        (panel) =>
-          (panelStates[panel] = currentExpanded < panel ? "end" : "start"),
-      );
+  const panelStates = writable<PanelStates>(defaultPanelStates);
+
+  export const getPanelState = (panelNumber: PanelNumber) =>
+    derived(panelStates, ($panelStates) => $panelStates[panelNumber]);
+
+  export const expandPanel = (expandPanel: PanelNumber) => {
+    let newPanelStates: PanelStates = { ...defaultPanelStates };
+
+    panels.forEach((panel) => {
+      if (panel !== expandPanel) {
+        newPanelStates[panel] = expandPanel < panel ? "end" : "start";
+      } else {
+        newPanelStates[panel] = "expanded";
+      }
+    });
+
+    panelStates.set(newPanelStates);
   };
+</script>
+
+<script lang="ts">
+  import Panel from "./Panel.svelte";
 </script>
 
 <div
   data-hero-grid
   class="m-auto flex h-full w-full max-w-screen-sm basis-auto flex-col p-3 lg:max-w-screen-xl lg:flex-row lg:items-center lg:p-5"
 >
-  <Panel
-    title="Welcome"
-    number={1}
-    bind:state={panelStates[1]}
-    on:hasExpanded={toggleOthers}
-  >
+  <Panel title="Welcome" number={1}>
     <slot name="panel-one" slot="panel" />
   </Panel>
 
-  <Panel
-    title="About Me"
-    number={2}
-    bind:state={panelStates[2]}
-    on:hasExpanded={toggleOthers}
-  >
+  <Panel title="About Me" number={2}>
     <slot name="panel-two" slot="panel" />
   </Panel>
 
-  <Panel
-    title="My Projects"
-    number={3}
-    bind:state={panelStates[3]}
-    on:hasExpanded={toggleOthers}
-  >
+  <Panel title="My Projects" number={3}>
     <slot name="panel-three" slot="panel" />
   </Panel>
 
-  <Panel
-    title="Contact Me"
-    number={4}
-    bind:state={panelStates[4]}
-    on:hasExpanded={toggleOthers}
-  >
+  <Panel title="Contact Me" number={4}>
     <slot name="panel-four" slot="panel" />
   </Panel>
 </div>
