fea1240c50ecabba47a0e89856b911f0c737ace7
Lukas Trombach <me@lukastrombach.dev>
2024-03-16 19:26:37 +1300

    add transforms and z-indices to hero grid

--- src/components/Hero/HeroGrid.svelte
+++ src/components/Hero/HeroGrid.svelte
@@ -29,6 +29,7 @@
 </script>
 
 <div
+  data-hero-grid
   class="m-auto flex h-full w-full max-w-screen-sm basis-auto flex-col p-3 lg:max-w-screen-xl lg:flex-row lg:items-center lg:p-5"
 >
   <Panel
@@ -67,3 +68,77 @@
     <slot name="panel-four" slot="panel" />
   </Panel>
 </div>
+
+<style lang="postcss" global>
+  [data-hero-grid] {
+    --translate-x: 0px;
+    --translate-y: 30px;
+  }
+
+  @media screen(lg) {
+    [data-hero-grid] {
+      --translate-x: 50px;
+      --translate-y: 0px;
+    }
+  }
+
+  [data-hero-grid] > :nth-child(1) {
+    @apply z-[4];
+  }
+
+  [data-hero-grid] > :nth-child(2) {
+    @apply z-[3];
+  }
+
+  [data-hero-grid] > :nth-child(3) {
+    @apply z-[2];
+  }
+
+  [data-hero-grid] > :nth-child(4) {
+    @apply z-[1];
+  }
+
+  [data-hero-grid] > :nth-child(1):is([data-state="start"]) {
+    @apply z-[1];
+  }
+
+  [data-hero-grid] > :nth-child(2):is([data-state="start"]) {
+    @apply z-[2];
+  }
+
+  [data-hero-grid] > :nth-child(3):is([data-state="start"]) {
+    @apply z-[3];
+  }
+
+  [data-hero-grid] > :nth-child(4):is([data-state="expanded"]) {
+    @apply z-[4];
+  }
+
+  [data-hero-grid] > :nth-child(2):is([data-state="start"]) {
+    --translate: translate(
+      calc(var(--translate-x) * -1),
+      calc(var(--translate-y) * -1)
+    );
+  }
+
+  [data-hero-grid] > :nth-child(3):is([data-state="start"]) {
+    --translate: translate(
+      calc(var(--translate-x) * -2),
+      calc(var(--translate-y) * -2)
+    );
+  }
+
+  [data-hero-grid] > :nth-child(2):is([data-state="end"]) {
+    --translate: translate(
+      calc(var(--translate-x) * 2),
+      calc(var(--translate-y) * 2)
+    );
+  }
+
+  [data-hero-grid] > :nth-child(3):is([data-state="end"]) {
+    --translate: translate(
+      calc(var(--translate-x) * 1),
+      calc(var(--translate-y) * 1)
+    );
+  }
+</style>
