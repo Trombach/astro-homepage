d7993ee513e2aab1b0a68b9f296b529d5d289702
Lukas Trombach <me@lukastrombach.dev>
2024-03-17 13:09:24 +1300

    add swipe handler to hero grid

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="svelte-gestures" />
 
 /* global astroHTML */
 
