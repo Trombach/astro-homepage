836d0e9f211bf9f3183d07121ebfd7d8a6583044
Lukas Trombach <me@lukastrombach.dev>
2024-03-24 14:41:42 +1300

    add cv.pdf endpoint

---
+++ src/pages/cv.pdf.ts
@@ -0,0 +1,30 @@
+import type { APIRoute } from "astro";
+import { google } from "googleapis";
+
+export const GET: APIRoute = async () => {
+  const fileId = import.meta.env.CV_FILE_ID;
+  const auth = import.meta.env.GOOGLE_API_KEY;
+
+  if (!fileId || !auth) {
+    throw new Error("Missing Google Drive configuration");
+  }
+
+  const drive = google.drive({
+    version: "v3",
+    auth,
+  });
+
+  try {
+    const { data } = await drive.files.get(
+      { fileId, alt: "media" },
+      { responseType: "arraybuffer" },
+    );
+
+    // @ts-expect-error TODO figure out how to do this without ts error
+    return new Response(Buffer.from(data));
+  } catch (err) {
+    return new Response("Error fetching data from Google Drive", {
+      status: 500,
+    });
+  }
+};
