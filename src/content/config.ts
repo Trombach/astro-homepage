5d3e3fdd2c2ee28f2d2b77ef0cbde8632e0e1dfa
Lukas Trombach <me@lukastrombach.dev>
2024-04-06 17:32:33 +1300

    update Project page layout and animations

--- src/content/config.ts
+++ src/content/config.ts
@@ -25,26 +25,31 @@ const tags = [
   "Computing",
 ] as const;
 
+const tools = ["astro", "svelte", "typescript", "tailwind", "vercel"] as const;
+
 const projectsCollection = defineCollection({
   type: "content",
   schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      location: z.string(),
-      image: z.string().optional(),
-      completed: z.coerce.date().optional(),
-      cover: image()
-        .optional()
-        .refine(
-          (img) =>
-            img?.width && img.format === "png" ? img.width >= 600 : true,
-          {
-            message: "Cover image must be at least 600 pixels wide!",
-          },
-        ),
-      tags: z.array(z.enum(tags)).optional(),
-    }),
+    z
+      .object({
+        title: z.string(),
+        description: z.string(),
+        repoLink: z.string().url().optional(),
+        image: z.string().optional(),
+        completed: z.coerce.date().optional(),
+        cover: image()
+          .optional()
+          .refine(
+            (img) =>
+              img?.width && img.format === "png" ? img.width >= 600 : true,
+            {
+              message: "Cover image must be at least 600 pixels wide!",
+            },
+          ),
+        tags: z.array(z.enum(tags)).optional(),
+        tools: z.array(z.enum(tools)).optional(),
+      })
+      .strict(),
 });
 
 export const collections = {
@@ -52,3 +57,5 @@ export const collections = {
   about: aboutCollection,
   projects: projectsCollection,
 };
+
+export type Tool = (typeof tools)[number];
