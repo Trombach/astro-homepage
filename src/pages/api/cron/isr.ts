8064e69befb277359876fd9309f5da9112afd371
Lukas Trombach <me@lukastrombach.dev>
2024-05-04 16:28:13 +1200

    remove cron auth for testing

--- src/pages/api/cron/isr.ts
+++ src/pages/api/cron/isr.ts
@@ -1,23 +1,10 @@
 import type { APIContext, APIRoute } from "astro";
-import { timingSafeEqual } from "node:crypto";
 
 const ENDPOINTS = ["/about"] as const;
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ request }: APIContext) => {
-  const authHeader = request.headers.get("authorization");
-  const secret = `Bearer ${import.meta.env.CRON_SECRET}`;
-
-  if (!authHeader) {
-    return new Response("Missing authentication", { status: 401 });
-  }
-
-  // use timingSafeEqual to prevent timing attacks
-  if (!timingSafeEqual(Buffer.from(authHeader), Buffer.from(secret))) {
-    return new Response("Access denied", { status: 401 });
-  }
-
   const bypassToken = import.meta.env.ISR_BYPASS_TOKEN;
 
   try {
