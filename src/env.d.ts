556d3dffbeff4668452e6c4c627e4b9716f3c3ff
Lukas Trombach <me@lukastrombach.dev>
2024-05-11 13:51:53 +1200

    use form action for contact form

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../.astro/actions.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="svelte-gestures" />
