8b971b3dbff2f46cc65026dfa261dfff21435181
Lukas Trombach <me@lukastrombach.dev>
2024-05-19 17:36:18 +1200

    switch CV to vercel blob storage

--- src/pages/cv.pdf.ts
+++ src/pages/cv.pdf.ts
@@ -1,32 +1,13 @@
 import type { APIRoute } from "astro";
-import { google } from "googleapis";
 
 export const prerender = false;
 
 export const GET: APIRoute = async () => {
-  const fileId = import.meta.env.CV_FILE_ID;
-  const auth = import.meta.env.GOOGLE_API_KEY;
-
-  if (!fileId || !auth) {
-    throw new Error("Missing Google Drive configuration");
+  if (!import.meta.env.CV_FILE_URL) {
+    return new Response("Missing file URL", { status: 500 });
   }
 
-  const drive = google.drive({
-    version: "v3",
-    auth,
-  });
+  const file = await fetch(import.meta.env.CV_FILE_URL);
 
-  try {
-    const { data } = await drive.files.get(
-      { fileId, alt: "media" },
-      { responseType: "arraybuffer" },
-    );
-
-    // @ts-expect-error TODO figure out how to do this without ts error
-    return new Response(Buffer.from(data));
-  } catch (err) {
-    return new Response("Error fetching data from Google Drive", {
-      status: 500,
-    });
-  }
+  return new Response(await file.arrayBuffer());
 };
