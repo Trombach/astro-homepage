0c9d786fb8c320b7827104538d63af9556ee38fa
Lukas Trombach <me@lukastrombach.dev>
2024-05-19 17:45:11 +1200

    minor fix to env type

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,6 +8,7 @@ interface ImportMetaEnv {
   VERCEL_ENV?: "production" | "preview" | "development";
   VERCEL_URL?: string;
   RESEND_TOKEN?: string;
+  ISR_BYPASS_TOKEN?: string;
   CV_FILE_URL?: string;
 }
 
