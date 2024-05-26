e3753ce23ab1ba84e2b7804b2d232048f82af5ca
Lukas Trombach <me@lukastrombach.dev>
2024-05-26 23:48:34 +1200

    add basic TOC

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,1 +1,13 @@
+import type { MarkdownHeading } from "astro";
+import type { Tool } from "src/content/config";
+
 export type ClassList = string | string[] | { [className: string]: boolean };
+export type ProjectProps = {
+  title: string;
+  headings: MarkdownHeading[];
+  description: string;
+  tools?: Tool[] | undefined;
+  repoLink?: string | undefined;
+  cover?: ImageMetadata | undefined;
+  coverAlt?: string | undefined;
+};
