d170cab4962f6acffee75be82431d5900638f22b
Lukas Trombach <me@lukastrombach.dev>
2024-06-16 11:39:27 +1200

    reconfigure CV endpoint
    - user separate variables for filename and baseurl

--- src/pages/cv.pdf.ts
+++ src/pages/cv.pdf.ts
@@ -3,11 +3,24 @@ import type { APIRoute } from "astro";
 export const prerender = false;
 
 export const GET: APIRoute = async () => {
-  if (!import.meta.env.CV_FILE_URL) {
-    return new Response("Missing file URL", { status: 500 });
+  if (!import.meta.env.VERCEL_STORAGE_URL) {
+    return new Response("Vercel storage URL not set", { status: 500 });
   }
 
-  const file = await fetch(import.meta.env.CV_FILE_URL);
+  if (!import.meta.env.CV_FILE_NAME) {
+    return new Response("Missing CV file name", { status: 500 });
+  }
 
-  return new Response(await file.arrayBuffer());
+  const file = await fetch(
+    new URL(import.meta.env.CV_FILE_NAME, import.meta.env.VERCEL_STORAGE_URL),
+  );
+
+  if (file.ok) {
+    return new Response(await file.arrayBuffer());
+  } else {
+    return new Response("Error fetching data: ", {
+      status: 500,
+      statusText: `${file.status} ${file.statusText}`,
+    });
+  }
 };
