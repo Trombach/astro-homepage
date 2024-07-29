041413665c76f320f059a7c656c7eb46656141cb
Lukas Trombach <me@lukastrombach.dev>
2024-07-29 19:51:04 +1200

    remove unnecessary `PUBLIC_` prefix

--- src/pages/cv.pdf.ts
+++ src/pages/cv.pdf.ts
@@ -1,22 +1,22 @@
 import type { APIRoute } from "astro";
 import {
-  PUBLIC_VERCEL_STORAGE_URL,
-  PUBLIC_CV_FILE_NAME,
+  VERCEL_STORAGE_URL,
+  CV_FILE_NAME,
 } from "astro:env/server";
 
 export const prerender = false;
 
 export const GET: APIRoute = async () => {
-  if (!PUBLIC_VERCEL_STORAGE_URL) {
+  if (!VERCEL_STORAGE_URL) {
     return new Response("Vercel storage URL not set", { status: 500 });
   }
 
-  if (!PUBLIC_CV_FILE_NAME) {
+  if (!CV_FILE_NAME) {
     return new Response("Missing CV file name", { status: 500 });
   }
 
   const file = await fetch(
-    new URL(PUBLIC_CV_FILE_NAME, PUBLIC_VERCEL_STORAGE_URL),
+    new URL(CV_FILE_NAME, VERCEL_STORAGE_URL),
   );
 
   if (file.ok) {
