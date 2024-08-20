9848fb151a8055682c8f03de3f063d10592b8b2c
Lukas Trombach <me@lukastrombach.dev>
2024-08-20 21:22:43 +0200

    apply linting fixes

--- src/components/ProjectPage/observe-headings.ts
+++ src/components/ProjectPage/observe-headings.ts
@@ -1,73 +1,74 @@
 const CURRENT_HEADING_ATTR = "data-current-heading";
 
 function setupObserver() {
-  const links = Array.from(
-    document.querySelector("nav#secondary")?.querySelectorAll("a") || [],
-  );
-  const articleHeadings =
-    document
-      .querySelector("article")
-      ?.querySelectorAll("h1, h2, h3, h4, h5, h6") || [];
-  const headings = Array.from(articleHeadings).filter(
-    (heading) => heading.id !== "",
-  );
-  const indicator = document.getElementById("heading-indicator");
+	const links = Array.from(
+		document.querySelector("nav#secondary")?.querySelectorAll("a") || [],
+	);
+	const articleHeadings =
+		document
+			.querySelector("article")
+			?.querySelectorAll("h1, h2, h3, h4, h5, h6") || [];
+	const headings = Array.from(articleHeadings).filter(
+		(heading) => heading.id !== "",
+	);
+	const indicator = document.getElementById("heading-indicator");
 
-  const handleIntersect = (entries: IntersectionObserverEntry[]) => {
-    //revert to initial state if no elements are intersecting and page is at the top
-    if (
-      indicator &&
-      entries.every((entry) => !entry.isIntersecting) &&
-      headings.every((heading) => heading.getBoundingClientRect().top > 0)
-    ) {
-      // reset indicator
-      indicator.style.removeProperty("opacity");
-      indicator.style.removeProperty("top");
+	const handleIntersect = (entries: IntersectionObserverEntry[]) => {
+		//revert to initial state if no elements are intersecting and page is at the top
+		if (
+			indicator &&
+			entries.every((entry) => !entry.isIntersecting) &&
+			headings.every((heading) => heading.getBoundingClientRect().top > 0)
+		) {
+			// reset indicator
+			indicator.style.removeProperty("opacity");
+			indicator.style.removeProperty("top");
 
-      // set all links inactive
-      links.forEach((link) => link.removeAttribute(CURRENT_HEADING_ATTR));
+			// set all links inactive
+			for (const link of links) {
+				link.removeAttribute(CURRENT_HEADING_ATTR);
+			}
 
-      return;
-    }
+			return;
+		}
 
-    // get first intersecting entry an set it as active
-    const entry = entries.filter(({ isIntersecting }) => isIntersecting)[0];
-    if (entry) {
-      const link = links.find((link) => link.hash === `#${entry.target.id}`);
+		// get first intersecting entry an set it as active
+		const entry = entries.filter(({ isIntersecting }) => isIntersecting)[0];
+		if (entry) {
+			const link = links.find((link) => link.hash === `#${entry.target.id}`);
 
-      if (link) {
-        // set active link
-        link.setAttribute(CURRENT_HEADING_ATTR, "");
-        if (indicator) {
-          indicator.style.opacity = "1";
-        }
+			if (link) {
+				// set active link
+				link.setAttribute(CURRENT_HEADING_ATTR, "");
+				if (indicator) {
+					indicator.style.opacity = "1";
+				}
 
-        // move indicator
-        const aside = document.getElementById("sidenav");
-        const top =
-          aside &&
-          link.getBoundingClientRect().top - aside.getBoundingClientRect().top;
+				// move indicator
+				const aside = document.getElementById("sidenav");
+				const top =
+					aside &&
+					link.getBoundingClientRect().top - aside.getBoundingClientRect().top;
 
-        if (indicator && top) {
-          indicator.style.top = `${top + 4}px`;
-        }
-      }
-      // set all other links as inactive
-      links.forEach(
-        (link) =>
-          link.hash !== `#${entry.target.id}` &&
-          link.removeAttribute(CURRENT_HEADING_ATTR),
-      );
-    }
-  };
+				if (indicator && top) {
+					indicator.style.top = `${top + 4}px`;
+				}
+			}
+			// set all other links as inactive
+			for (const link of links) {
+				link.hash !== `#${entry.target.id}` &&
+					link.removeAttribute(CURRENT_HEADING_ATTR);
+			}
+		}
+	};
 
-  if (headings.length > 0) {
-    const observer = new IntersectionObserver(handleIntersect, {
-      rootMargin: "0% 0% -85% 0%",
-      threshold: 0,
-    });
-    headings.forEach((heading) => observer.observe(heading));
-  }
+	if (headings.length > 0) {
+		const observer = new IntersectionObserver(handleIntersect, {
+			rootMargin: "0% 0% -85% 0%",
+			threshold: 0,
+		});
+		for (const heading of headings) observer.observe(heading);
+	}
 }
 
 setupObserver();
--- src/utils/fetchHelper.ts
+++ src/utils/fetchHelper.ts
@@ -4,51 +4,52 @@ import type { z } from "astro/zod";
 import { VERCEL_URL } from "astro:env/server";
 
 export default async function fetchWithSchema<S extends z.ZodTypeAny>(
-  schema: S,
-  input: URL | string,
-  init?: RequestInit,
+	schema: S,
+	input: URL | string,
+	init?: RequestInit,
 ): Promise<z.infer<S> | z.ZodError> {
-  if (typeof input === "string") {
-    input = getURL(input);
-  }
+	const inp = typeof input === "string" ? getURL(input) : input;
+	const response = await fetch(inp, init);
 
-  const response = await fetch(input, init);
+	if (!response.ok) {
+		throw new Error(
+			`Error fetching data from ${inp}: ${response.status} ${response.statusText}`,
+		);
+	}
 
-  if (!response.ok) {
-    throw new Error(
-      `Error fetching data from ${input}: ${response.status} ${response.statusText}`,
-    );
-  }
+	try {
+		const json = await response.json();
+		const parsed = schema.safeParse(json);
 
-  try {
-    const json = await response.json();
-    const parsed = schema.safeParse(json);
+		if (!parsed.success) {
+			console.error(parsed.error);
+			return parsed.error;
+		}
 
-    if (!parsed.success) {
-      console.error(parsed.error);
-      return parsed.error;
-    }
-
-    return parsed.data;
-  } catch (err) {
-    console.log(err);
-    return;
-  }
+		return parsed.data;
+	} catch (err) {
+		console.log(err);
+		return;
+	}
 }
 
-function getURL(url: URL | string, searchParams?: { [key: string]: string }) {
-  if (typeof url === "string" && url.startsWith("http")) {
-    url = new URL(url);
-  } else {
-    const base = VERCEL_URL ? `https://${VERCEL_URL}` : "http://localhost:4321";
-    url = new URL(url, base);
-  }
+function getURL(
+	inputUrl: URL | string,
+	searchParams?: { [key: string]: string },
+) {
+	let url =
+		typeof inputUrl === "string" && inputUrl.startsWith("http")
+			? new URL(inputUrl)
+			: new URL(
+					inputUrl,
+					VERCEL_URL ? `https://${VERCEL_URL}` : "http://localhost:4321",
+				);
 
-  if (searchParams) {
-    url = new URL(
-      `${url.origin}${url.pathname}${new URLSearchParams({ ...url.searchParams, ...searchParams })}`,
-    );
-  }
+	if (searchParams) {
+		url = new URL(
+			`${url.origin}${url.pathname}${new URLSearchParams({ ...url.searchParams, ...searchParams })}`,
+		);
+	}
 
-  return url;
+	return url;
 }
