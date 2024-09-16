0c2d4c803b4c642d596d111723d8421f3a89ff97
Lukas Trombach <me@lukastrombach.dev>
2024-09-16 20:25:37 +0200

    create vcf file from collection entry

--- src/pages/lukas-trombach.vcf.ts
+++ src/pages/lukas-trombach.vcf.ts
@@ -1,18 +1,27 @@
-const vcfContent = `BEGIN:VCARD
-VERSION:3.0
-FN;CHARSET=UTF-8:Lukas Trombach
-N;CHARSET=UTF-8:Trombach;Lukas;;;
-EMAIL;CHARSET=UTF-8;type=HOME,INTERNET:me@lukastrombach.dev
-TEL;TYPE=CELL:+4915140324079
-ADR;CHARSET=UTF-8;TYPE=HOME:;;;Kiel;Schleswig-Holstein;24143;Deutschland
-URL;CHARSET=UTF-8:https://lukastrombach.dev
-X-SOCIALPROFILE;TYPE=linkedin:https://www.linkedin.com/in/lukas-trombach/
-REV:2024-08-27T11:44:10.098Z
-END:VCARD
-`;
+import { getEntry } from "astro:content";
+import { getLatestCommitDate } from "@it-astro:content/git";
 
-export function GET() {
-  return new Response(vcfContent, {
-    headers: { "Content-Type": "text/vcard" },
-  });
+export async function GET() {
+  const person = await getEntry("person", "lukas");
+  const { data } = person;
+  const [firstName, lastName] = data.name.split(" ");
+  const { city, region, zip, country } = data.currentLocation;
+  const revision = await getLatestCommitDate(person);
+
+  return new Response(
+    `BEGIN:VCARD
+VERSION:3.0
+FN;CHARSET=UTF-8:${data.name}
+N;CHARSET=UTF-8:${lastName};${firstName};;;
+EMAIL;CHARSET=UTF-8;type=HOME,INTERNET:${data.contact.email}
+ADR;CHARSET=UTF-8;TYPE=HOME:;;;${city};${region};${zip};${country}
+URL;CHARSET=UTF-8:${data.contact.web}
+X-SOCIALPROFILE;TYPE=linkedin:${data.contact.linkedIn}
+REV:${revision.toISOString()}
+END:VCARD
+`,
+    {
+      headers: { "Content-Type": "text/vcard" },
+    },
+  );
 }
