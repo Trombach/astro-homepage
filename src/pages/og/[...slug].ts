97a86af23c2f4475caeca9258801ad428be70b26
Lukas Trombach <me@lukastrombach.dev>
2024-09-23 11:56:14 +0200

    add og image endpoint

---
+++ src/pages/og/[...slug].ts
@@ -0,0 +1,45 @@
+import { OGImageRoute } from "astro-og-canvas";
+import { getCollection } from "astro:content";
+
+const projects = await getCollection("projects");
+const pages = Object.fromEntries(
+  projects.map(({ id, data: { title, description } }) => [
+    id,
+    { title, description },
+  ]),
+);
+pages.landing = { title: "lukastrombach.dev", description: "Lukas Trombach" };
+pages.projects = {
+  title: "My Projects",
+  description: "What I've been working on",
+};
+pages.contact = {
+  title: "Get in touch",
+  description: "contact@lukastrombach.dev",
+};
+
+export const { getStaticPaths, GET } = OGImageRoute({
+  param: "slug",
+  pages,
+  getImageOptions: (_path, page: (typeof pages)[number]) => {
+    return {
+      format: "WEBP",
+      title: page.title,
+      description: page.description,
+      bgImage: {
+        path: "./src/images/og-image.png",
+        fit: "contain",
+        position: "center",
+      },
+      border: { color: [124, 58, 237], side: "inline-start", width: 16 },
+      logo: { path: "./src/images/logo.png", size: [250] },
+      font: {
+        title: { families: ["Inter"] },
+        description: { families: ["Inter"] },
+      },
+      fonts: [
+        "https://cdn.jsdelivr.net/fontsource/fonts/inter:vf@latest/latin-wght-normal.woff2",
+      ],
+    };
+  },
+});
