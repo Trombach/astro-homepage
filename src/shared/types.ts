ca14a0c6c79e36b6a15dac3284daebda0e63017d
Lukas Trombach <me@lukastrombach.dev>
2024-10-23 17:36:55 +0200

    remove unused style

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,7 +1,6 @@
 import type { CollectionEntry } from "astro:content";
 import type { MarkdownHeading } from "astro";
 
-export type ClassList = string | string[] | { [className: string]: boolean };
 export type ProjectProps = CollectionEntry<"projects">["data"] & {
   headings: MarkdownHeading[];
 };
