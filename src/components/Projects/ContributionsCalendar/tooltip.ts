991b96aa2e9eb3a3138f88b8b7d496528e3734e4
Lukas Trombach <me@lukastrombach.dev>
2025-01-26 11:55:13 +0100

    use linkedom to set attributes on elements with tooltips

--- src/components/Projects/ContributionsCalendar/tooltip.ts
+++ src/components/Projects/ContributionsCalendar/tooltip.ts
@@ -26,19 +26,7 @@ async function init() {
         return;
       }
 
-      // remove old content
-      while (
-        tooltipElement.lastChild?.nodeType !== Node.COMMENT_NODE &&
-        tooltipElement.lastChild?.nodeValue !== "[tooltip content]"
-      ) {
-        tooltipElement.lastChild?.remove();
-      }
-
-      const comment = tooltipElement.lastChild;
-      const template = anchor.querySelector("template");
-      comment.after(
-        template?.content.cloneNode(true) ?? document.createTextNode(""),
-      );
+      updateTooltip(tooltipElement, anchor);
 
       const { x, y, placement, middlewareData } = await computePosition(
         anchor,
@@ -58,36 +46,34 @@ async function init() {
         top: `${y}px`,
       });
 
-      if (!middlewareData.arrow || !(arrowElement instanceof HTMLElement)) {
-        return;
+      if (middlewareData.arrow && arrowElement instanceof HTMLElement) {
+        const key = placement.split("-")[0];
+
+        if (!key) {
+          return;
+        }
+
+        const staticSide = {
+          top: "bottom",
+          right: "left",
+          bottom: "top",
+          left: "right",
+        }[key];
+
+        if (!staticSide) {
+          return;
+        }
+
+        const { x: arrowX, y: arrowY } = middlewareData.arrow;
+
+        Object.assign(arrowElement.style, {
+          left: arrowX != null ? `${arrowX}px` : "",
+          top: arrowY != null ? `${arrowY}px` : "",
+          right: "",
+          bottom: "",
+          [staticSide]: "-4px",
+        });
       }
-
-      const key = placement.split("-")[0];
-
-      if (!key) {
-        return;
-      }
-
-      const staticSide = {
-        top: "bottom",
-        right: "left",
-        bottom: "top",
-        left: "right",
-      }[key];
-
-      if (!staticSide) {
-        return;
-      }
-
-      const { x: arrowX, y: arrowY } = middlewareData.arrow;
-
-      Object.assign(arrowElement.style, {
-        left: arrowX != null ? `${arrowX}px` : "",
-        top: arrowY != null ? `${arrowY}px` : "",
-        right: "",
-        bottom: "",
-        [staticSide]: "-4px",
-      });
     }
 
     function showTooltip() {
@@ -115,3 +101,32 @@ async function init() {
 }
 
 await init();
+
+function updateTooltip(tooltipElement: HTMLElement, anchor: HTMLElement) {
+  // remove old content
+  while (
+    tooltipElement.lastChild?.nodeType !== Node.COMMENT_NODE &&
+    tooltipElement.lastChild?.nodeValue !== "[tooltip content]"
+  ) {
+    tooltipElement.lastChild?.remove();
+  }
+
+  // insert new content
+  const comment = tooltipElement.lastChild;
+
+  let tooltipContent: Node | string | undefined = anchor.dataset.tooltipText;
+
+  if (!tooltipContent) {
+    if (!anchor.dataset.tooltipId) {
+      return;
+    }
+    const template = document.getElementById(anchor.dataset.tooltipId);
+
+    if (!(template instanceof HTMLTemplateElement)) {
+      return;
+    }
+    tooltipContent ??= template.content.cloneNode(true);
+  }
+
+  comment.after(tooltipContent);
+}
