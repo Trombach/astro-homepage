211c5ece283d8c4b8ec1de819992268e69143212
Lukas Trombach <me@lukastrombach.dev>
2025-05-01 17:50:30 +0200

    beasties middleware + integration

---
+++ src/shared/beasties.ts
@@ -0,0 +1,28 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { outDir } from "astro:config/server";
+import type { Options as BeastiesOptions } from "beasties";
+import Beasties from "beasties";
+
+const options = {
+  pruneSource: false,
+  inlineFonts: true,
+  allowRules: [":root.dark"],
+  mergeStylesheets: false,
+  reduceInlineStyles: false,
+} as const satisfies BeastiesOptions;
+
+const beastiesSSR = new Beasties({
+  path: path.join(fileURLToPath(outDir), "client"),
+  ...options,
+});
+
+const beastiesStatic = new Beasties({
+  ...options,
+});
+
+export { options, beastiesSSR, beastiesStatic };
+export default new Beasties({
+  path: path.join(fileURLToPath(outDir), "client"),
+  ...options,
+});
